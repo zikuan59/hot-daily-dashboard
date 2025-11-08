1d9e3fcd75e07344b29682a096e96ce88dd8f665
zikuan59 <venturajrelpidio@gmail.com>
2025-11-08 21:44:11 +0800

    fix: switch to Vue + Vite setup

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,13 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [vue()],
   build: {
     outDir: 'dist'
+  },
+  server: {
+    port: 5173
   }
 })

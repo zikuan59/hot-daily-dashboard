e92cc0e5ca9d33d285a041d6dfb5dceb9a103299
zikuan59 <venturajrelpidio@gmail.com>
2025-11-08 21:51:27 +0800

    fix: set Vite base path for Vercel

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: './',  // ✅ 这一行非常关键
   build: {
     outDir: 'dist'
   },
